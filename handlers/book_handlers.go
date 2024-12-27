133ac6f6925426db487df45766a94656a00c5779
Caterwaulin <sea.gilbert002@gmail.com>
2024-12-26 19:38:44 -0500

    created a git ignore and continued working on my handlers

--- handlers/book_handlers.go
+++ handlers/book_handlers.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-
 )
 
 type Server struct {
@@ -40,7 +39,7 @@ func (s *Server) IndexHandler(w http.ResponseWriter, r *http.Request) {
     tmpl.Execute(w, nil)
 }
 
-// Handles the books page for inserting a new book or more
+// Handles the books page for diplaying the books in the database
 func (s *Server) BooksHandler(w http.ResponseWriter, r *http.Request) {
     rows, err := s.DB.Query("SELECT id, title, author, publish_date, location FROM books")
     if err != nil {
@@ -56,11 +55,11 @@ func (s *Server) BooksHandler(w http.ResponseWriter, r *http.Request) {
         rows.Scan(&id, &title, &author, &publishDate, &location)
 
         books = append(books, map[string]string {
-            "id": fmt.Sprint(id),
-            "title": title,
-            "author": author,
-            "publishDate": publishDate,
-            "location": location,
+            "id":           fmt.Sprint(id),
+            "title":        title,
+            "author":       author,
+            "publishDate":  publishDate,
+            "location":     location,
         })
     }
 
@@ -71,3 +70,40 @@ func (s *Server) BooksHandler(w http.ResponseWriter, r *http.Request) {
     }
     tmpl.Execute(w, books)
 }
+
+// A handler for adding a book to the database
+func (s *Server) AddBookHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodGet {
+        tmpl, err := template.ParseFiles("templates/add_book.html")
+        if err != nil {
+            http.Error(w, "Error rendering template", http.StatusInternalServerError)
+            return
+        }
+        tmpl.Execute(w, nil)
+    } else if r.Method == http.MethodPost {
+        err := r.ParseForm()
+        if err != nil {
+            http.Error(w, "Error parsing book form", http.StatusBadRequest)
+            return
+        }
+        title :=        r.FormValue("title"),
+        author :=        r.FormValue("author"),
+        firstName
+        lastName        string
+        genre           string
+        series          string
+        description     string
+        publishDate     string
+        publisher       string
+        ean_isbn        string
+        upc_isbn        string
+        pages           int32
+        ddc             string
+        coverStyle      string
+        sprayedEdges    bool
+        specialEd       bool
+        firstEd         bool
+        signed          bool
+        location        bool
+    }
+
