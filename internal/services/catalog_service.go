25866f747961436e6b9402a32e7179260a43e1df
Chrysanthemum Gribble Gilbert <sea.gilbert002@gmail.com>
2025-11-22 12:16:20 -0500

    Add service logic to parse form

--- internal/services/catalog_service.go
+++ internal/services/catalog_service.go
@@ -1,6 +1,12 @@
 package services
 
-import "github.com/seagilbert002/LittleLibrary/internal/models"
+import (
+	"errors"
+	"net/url"
+	"strconv"
+
+	"github.com/seagilbert002/LittleLibrary/internal/models"
+)
 
 // Repository interface for calling to the database
 type BookRepository interface {
@@ -31,6 +37,55 @@ func (s *CatalogService) GetBookById(id int) (*models.Book, error) {
 	return s.Repo.GetBookById(id)
 }
 
-func (s *CatalogService) AddBook(book models.Book) (error) {
+func (s *CatalogService) AddBook(bookData url.Values) error {
+	// Validate the required fields
+	if bookData.Get("title") == "" {
+		return errors.New("book title is required")
+	}
+	if bookData.Get("author") == "" {
+		return errors.New("book author is required")
+	}
+	if bookData.Get("first_name") == "" {
+		return errors.New("book First Name is required")
+	}
+	if bookData.Get("last_name") == "" {
+		return errors.New("book Last Name is required")
+	}
+
+	// Transform data if needed here
+	pagesStr := bookData.Get("pages")
+	var pages uint16
+
+	if pagesStr != "" {
+		p, err := strconv.ParseUint(pagesStr, 10, 16)
+		if err != nil {
+			return errors.New("pages must be a valid number")
+		}
+		pages = uint16(p)
+	}
+
+	// Assembling the Model
+	book := models.Book{
+		Title: 			bookData.Get("title"),
+		Author:			bookData.Get("author"),
+		AuthorFirst:	bookData.Get("first_name"),
+		AuthorLast:		bookData.Get("last_name"),
+		Genre:			bookData.Get("genre"),
+		Series:			bookData.Get("series"),
+		Description:	bookData.Get("description"),
+		PublishDate:	bookData.Get("publish_date"),
+		Publisher:		bookData.Get("publisher"),
+		EanIsbn:		bookData.Get("ean_isbn"),
+		UpcIsbn:		bookData.Get("upc_isbn"),
+		Pages:			pages,
+		Ddc:			bookData.Get("ddc"),
+		CoverStyle:		bookData.Get("cover_style"),
+		SprayedEdges:	bookData.Get("sprayed_edges") == "on",
+		SpecialEd:		bookData.Get("special_ed") == "on",
+		FirstEd:		bookData.Get("first_ed") == "on",
+		Signed:			bookData.Get("signed") == "on",
+		Location:		bookData.Get("location"),
+	}
+
 	return s.Repo.AddBook(book)
 }
